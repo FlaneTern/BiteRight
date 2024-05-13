08372b079596e8fd0307a3632cea47cefc41226c
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-05-13 10:51:15 +0700

    Revert "Revert "readjust scan button on bottom nav""
    This reverts commit 81641bddef85d9382420a293116b5f573c2b75cf.

--- BiteRightApp/TODO.md
+++ BiteRightApp/TODO.md
@@ -2,7 +2,7 @@
 
 - [X] Get Bottom Tabs Navigation working
 - [ ] Add more pages to stack nav
-- [ ] Readjust camera position on bottom nav once the design is finalized
+- [X] Readjust camera position on bottom nav once the design is finalized
 - [X] Link to camera
 - [ ] Add overlay to camera for barcode scanning
 - [ ] Make Landing page as default when first opening the app
--- BiteRightApp/app/(tabs)/_layout.tsx
+++ BiteRightApp/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
-import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Tabs, useNavigation, useSegments } from "expo-router";
-import { TouchableOpacity } from "react-native";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function AppLayout() {
   const segments = useSegments();
@@ -10,57 +11,80 @@ export default function AppLayout() {
   const navigation = useNavigation();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "blue",
-        tabBarStyle: {
-          height: 60,
-          display: pagesToHideTabs.includes(page) ? "none" : "flex",
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          marginBottom: 8,
-          marginTop: -8,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="home" color={color} size={28} />
-          ),
+    <SafeAreaProvider>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "black",
+          tabBarStyle: {
+            height: 60,
+            display: pagesToHideTabs.includes(page) ? "none" : "flex",
+          },
+          tabBarLabelStyle: {
+            fontSize: 12.5,
+            fontWeight: "bold",
+            marginBottom: 8,
+            marginTop: -8,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="scan"
-        options={{
-          title: "Scan",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="camera" color={color} size={22} />
-          ),
-          headerLeft: () => (
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              style={{ marginLeft: 20 }}
-            >
-              <AntDesign name="arrowleft" size={24} color="white" />
-            </TouchableOpacity>
-          ),
-          headerTransparent: true,
-          headerTitle: "",
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="gear" color={color} size={24} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="home" color={color} size={28} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="scan"
+          options={{
+            title: "Scan",
+            tabBarIcon: ({ color }) => (
+              <View style={styles.scanButton}>
+                <FontAwesome name="camera" color={color} size={22} />
+                <Text style={{ fontWeight: "bold", fontSize: 12.5 }}>Scan</Text>
+              </View>
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={{ marginLeft: 20 }}
+              >
+                <AntDesign name="arrowleft" size={28} color="white" />
+              </TouchableOpacity>
+            ),
+            headerTransparent: true,
+            headerTitle: "",
+            tabBarLabel: "",
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="gear" color={color} size={24} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  scanButton: {
+    width: 70,
+    height: 70,
+    backgroundColor: "yellow",
+    borderRadius: 35,
+    borderColor: "black",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    marginBottom: 40,
+  },
+});
