3a1567b2cf3472339520207c5be1ad1ed4dccf9c
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-06-13 12:57:02 +0700

    feat: added Diet and Daily Calorie Limit page

--- BiteRightApp/app/(auth)/(settings)/diet.tsx
+++ BiteRightApp/app/(auth)/(settings)/diet.tsx
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { defaultStyles } from "@/constants/Styles";
 
 const DietPage = () => {
   return (
-    <View>
-      <Text>DietPage</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Text style={styles.body}>
+        We make sure to cater to your dietary preference. Please tick boxes that
+        apply.
+      </Text>
     </View>
   );
 };
 
 export default DietPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  body: {
+    ...defaultStyles.body,
+    width: "85%",
+    alignSelf: "center",
+    textAlign: "center",
+    marginVertical: 32,
+    fontSize: 12.5,
+  },
+});
